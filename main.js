57e64f5c93b624fa1296abdf6cf415fa38174d95
yutveg <kodi.0n31l@gmail.com>
2020-06-23 11:51:31 -0700

    attribute binding works

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
+    link: "http://google.com",
     image: "./assets/socks.png",
     description: "These clothy foot containers will make you feel splendid.",
   },
