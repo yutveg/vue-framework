1f92c4b47368ebefdcb9127013b712f99c8c925a
yutveg <kodi.0n31l@gmail.com>
2020-06-23 11:37:26 -0700

    basic vue concepts

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        product: 'Socks'
-    }
-})
+  el: "#app",
+  data: {
+    product: "Socks",
+    image: "./assets/socks.png",
+    description: "These clothy foot containers will make you feel splendid.",
+  },
+});
