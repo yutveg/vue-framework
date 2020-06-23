f4de4c3c28eda6de10aacbb55c21a5116f6abc49
yutveg <kodi.0n31l@gmail.com>
2020-06-23 12:30:31 -0700

    event handling

--- main.js
+++ main.js
@@ -2,8 +2,25 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
+    inStock: false,
     link: "http://google.com",
     image: "./assets/socks.png",
     description: "These clothy foot containers will make you feel splendid.",
+    details: ["organic", "fresh", "stupid", "ugly"],
+    variants: [
+      {
+        id: 1,
+        color: "blue",
+      },
+      {
+        id: 2,
+        color: "pink",
+      },
+    ],
+  },
+  methods: {
+    toggleStock: function () {
+      this.inStock = !this.inStock;
+    },
   },
 });
