44ea2419dda54975a07433be5008a6984e9e8f05
yutveg <kodi.0n31l@gmail.com>
2020-06-26 09:43:27 -0700

    contact route to test form up

---
+++ my-app/src/views/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <label>Name</label>
+    <input v-model="input" value="name" />
+    <label>Message</label>
+    <input v-model="input" value="message" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      message: ""
+    };
+  }
+};
+</script>
