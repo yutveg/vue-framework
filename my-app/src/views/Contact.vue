89022a00d010caea40c6630ca78b277127505613
yutveg <kodi.0n31l@gmail.com>
2020-07-01 11:18:46 -0700

    built out card create method on vue component

--- my-app/src/views/Contact.vue
+++ my-app/src/views/Contact.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    <label>Name</label>
-    <input v-model="form.name" type="text" name="name" />
-    <label>Message</label>
-    <input v-model="form.message" type="text" name="message" />
+    <form @submit="createCard">
+      <label>Name</label>
+      <input v-model="form.name" type="text" name="name" />
+      <label>Message</label>
+      <input v-model="form.message" type="text" name="message" />
+      <button type="submit">Submit</button>
+    </form>
+
+    <div v-for="card in cards" :key="card.name">
+      <h1>{{ card.name }}</h1>
+      <h2>{{ card.message }}</h2>
+    </div>
   </div>
 </template>
 
@@ -14,8 +22,14 @@ export default {
       form: {
         name: "",
         message: ""
-      }
+      },
+      cards: []
     };
+  },
+  methods: {
+    createCard(e, cardData) {
+      this.cards.push(cardData);
+    }
   }
 };
 </script>
