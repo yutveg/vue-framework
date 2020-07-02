ad7b33aa211271454360a2f4527a520f9c25f33a
yutveg <kodi.0n31l@gmail.com>
2020-07-02 10:39:19 -0700

    refactored contact into component with an import

---
+++ my-app/src/components/Contact.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form @submit="createCard">
+      <label>Name</label>
+      <input v-model="form.name" type="text" name="name" />
+      <label>Message</label>
+      <input v-model="form.message" type="text" name="message" />
+      <button type="submit">Submit</button>
+    </form>
+
+    <div v-for="card in cards" :key="card.name">
+      <h1>{{ card.name }}</h1>
+      <h2>{{ card.message }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        message: ""
+      },
+      cards: []
+    };
+  },
+  methods: {
+    createCard(e) {
+      e.preventDefault();
+      let card = {
+        name: this.form.name,
+        message: this.form.message
+      };
+      this.cards.push(card);
+      this.form.name = "";
+      this.form.message = "";
+    }
+  }
+};
+</script>
--- my-app/src/views/Contact.vue
+++ my-app/src/views/Contact.vue
@@ -1,42 +1,14 @@
 <template>
   <div>
-    <form @submit="createCard">
-      <label>Name</label>
-      <input v-model="form.name" type="text" name="name" />
-      <label>Message</label>
-      <input v-model="form.message" type="text" name="message" />
-      <button type="submit">Submit</button>
-    </form>
-
-    <div v-for="card in cards" :key="card.name">
-      <h1>{{ card.name }}</h1>
-      <h2>{{ card.message }}</h2>
-    </div>
+    <h1>Contact Form</h1>
+    <Contact />
   </div>
 </template>
 
 <script>
+import Contact from "@/components/Contact.vue";
+
 export default {
-  data() {
-    return {
-      form: {
-        name: "",
-        message: ""
-      },
-      cards: []
-    };
-  },
-  methods: {
-    createCard(e) {
-      e.preventDefault();
-      let card = {
-        name: this.form.name,
-        message: this.form.message
-      };
-      this.cards.push(card);
-      this.form.name = "";
-      this.form.message = "";
-    }
-  }
+  name: "Contact"
 };
 </script>
